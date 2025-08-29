45098f70c3dbf7b1c3dafee433e1886072d77344
Shalva Chubinidze <chubinidze.shalva@gmail.com>
2025-08-29 12:47:34 +0400

    chore(config): update application configuration and dependencies

--- services/api/src/main/java/com/chubini/pku/PkuApiApplication.java
+++ services/api/src/main/java/com/chubini/pku/PkuApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.chubini.pku")
-@EntityScan("com.chubini.pku.products")
-@EnableJpaRepositories("com.chubini.pku.products")
+@EntityScan("com.chubini.pku")
+@EnableJpaRepositories("com.chubini.pku")
 public class PkuApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PkuApiApplication.class, args);
