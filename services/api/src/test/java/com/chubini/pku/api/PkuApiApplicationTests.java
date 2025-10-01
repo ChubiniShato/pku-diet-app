7b6435f890b36033499f142ecb473f6ef6fef643
ChubiniShato <ChubiniShato@users.noreply.github.com>
2025-10-01 19:03:58 +0400

    test: make contextLoads DB/Flyway-free for CI (exclude DataSource/JPA/Flyway autoconfig)

--- services/api/src/test/java/com/chubini/pku/api/PkuApiApplicationTests.java
+++ services/api/src/test/java/com/chubini/pku/api/PkuApiApplicationTests.java
@@ -1,10 +1,22 @@
 package com.chubini.pku.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    // არ გაუშვა DB/JPA/Flyway ავტოკონფიგები კონტექსტ-ლოდისთვის
+    "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class PkuApiApplicationTests {
-  @Test
-  void contextLoads() {}
+
+    @org.junit.jupiter.api.Test
+    void contextLoads() {
+        // just boot context
+    }
 }
