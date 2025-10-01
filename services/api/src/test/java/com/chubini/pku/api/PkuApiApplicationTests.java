5762b96f5225c19c804ae268139003a434fb23a0
ChubiniShato <ChubiniShato@users.noreply.github.com>
2025-10-01 19:56:01 +0400

    test: lighten contextLoads smoke test; exclude SecurityAutoConfiguration and use webEnvironment=NONE

--- services/api/src/test/java/com/chubini/pku/api/PkuApiApplicationTests.java
+++ services/api/src/test/java/com/chubini/pku/api/PkuApiApplicationTests.java
@@ -4,19 +4,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-    // არ გაუშვა DB/JPA/Flyway ავტოკონფიგები კონტექსტ-ლოდისთვის
-    "spring.autoconfigure.exclude=" +
-        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
-})
+@TestPropertySource(
+    properties = {
+      // არ გაუშვა DB/JPA/Flyway/Security ავტოკონფიგები კონტექსტ-ლოდისთვის
+      "spring.autoconfigure.exclude="
+          + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration,"
+          + "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+    })
 class PkuApiApplicationTests {
 
-    @org.junit.jupiter.api.Test
-    void contextLoads() {
-        // just boot context
-    }
+  @org.junit.jupiter.api.Test
+  void contextLoads() {
+    // just boot context
+  }
 }
